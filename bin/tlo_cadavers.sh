a2f88d6517165a54e9baa8994ee62522ee183fda
Bruce Bye <feliks13@gmail.com>
2016-06-01 21:52:45 +0100

    Idling cadaver and zone handling brought into main action set

--- bin/tlo_cadavers.sh
+++ bin/tlo_cadavers.sh
@@ -62,8 +62,8 @@ function deduce_zone_index
   #  elif test_game_contents select_map_wasteland.png $TEST_LEFT_ZONE_INDICATOR
   #  then
   #    current_zone_index=13
-#    else
-#      t_echo "Could not deduce zone index!"
+    else
+      t_echo "Could not deduce zone index!"
     fi
   else
 #    t_echo "Not in select map view"
@@ -132,47 +132,12 @@ function select_zone
 
   if (($present_zone != $destination_index))
   then
-    select_zone_actions+=('
-      condition="! ((\$in_select_map))"
-      sqmessage="Opening zone selection"
-      action="xdo_and_return mousemove $CLICK_OPEN_ZONE_SELECT click 1"
-    ')
-
-    # Shifting actions have an extra short sleep, to minimise the risk of over-shifting
-    select_zone_actions+=('
-      condition="((\$in_select_map)) && ((\$current_zone_index > $destination_index))"
-      sqmessage="Shifting left"
-      action="xdo_and_return mousemove $CLICK_ZONES_LEFT_SHIFT click 1 sleep 1"
-      repeattoclear=1
-    ')
-
-    select_zone_actions+=('
-      condition="((\$in_select_map)) && ((\$current_zone_index < $destination_index - 1))"
-      sqmessage="Shifting right"
-      action="xdo_and_return mousemove $CLICK_ZONES_RIGHT_SHIFT click 1 sleep 1"
-      repeattoclear=1
-    ')
-
-    select_zone_actions+=('
-      condition="((\$in_select_map)) && ((\$current_zone_index == $destination_index))"
-      sqmessage="Selecting left"
-      action="xdo_and_return mousemove $CLICK_ZONES_LEFT_SELECT click 1"
-      finalaction=1
-    ')
-
-    select_zone_actions+=('
-      condition="((\$in_select_map)) && ((\$current_zone_index == $destination_index - 1))"
-      sqmessage="Selecting right"
-      action="xdo_and_return mousemove $CLICK_ZONES_RIGHT_SELECT click 1"
-      finalaction=1
-    ')
-
-    select_zone_actions+=("${interrupt_actions[@]}")
-
-    run_actions select_zone_actions[@] deduce_zone_index
-    present_zone=$destination_index
+    sq_echo "Opening zone selection"
+    xdo_and_return mousemove $CLICK_OPEN_ZONE_SELECT click 1
+    return 1
   else
     sq_echo "Already in the destination zone"
+    return 0
   fi
 }
 
@@ -186,157 +151,158 @@ function start_cadaver
 
   case $cadaver in
     Cook)
-      select_zone Downtown
+      zone_name="Downtown"
       ;;
     Alzheimer)
-      select_zone Downtown
+      zone_name="Downtown"
       ;;
     MissusMechanic)
-      select_zone Downtown
+      zone_name="Downtown"
       ;;
     Firefighter)
-      select_zone Downtown
+      zone_name="Downtown"
       ;;
     Surgeon)
-      select_zone Downtown
+      zone_name="Downtown"
       ;;
     Sheriff)
-      select_zone Downtown
+      zone_name="Downtown"
       ;;
     Electrician)
-      select_zone Industrial
+      zone_name="Industrial"
       ;;
     Concreter)
-      select_zone Industrial
+      zone_name="Industrial"
       ;;
 
     RoadWorker)
-      select_zone Industrial
+      zone_name="Industrial"
       ;;
     TwinFriend)
-      select_zone Industrial
+      zone_name="Industrial"
       ;;
 
     Hauler)
-      select_zone Industrial
+      zone_name="Industrial"
       ;;
     Trackwalker)
-      select_zone Industrial
+      zone_name="Industrial"
       ;;
 
     Farmer)
-      select_zone Suburbs
+      zone_name="Suburbs"
       ;;
     Exterminator)
-      select_zone Suburbs
+      zone_name="Suburbs"
       ;;
 
     Defroster)
-      select_zone Suburbs
+      zone_name="Suburbs"
       ;;
     DeadBaron)
-      select_zone Suburbs
+      zone_name="Suburbs"
       drops_gear=0
       ;;
 
     Milligans)
-      select_zone Suburbs
+      zone_name="Suburbs"
       ;;
     Olympia)
-      select_zone Suburbs
+      zone_name="Suburbs"
       ;;
 
     Avenger)
-      select_zone City
+      zone_name="City"
       drops_gear=0
       ;;
     Archivist)
-      select_zone City
+      zone_name="City"
       ;;
 
     StewardAbraham)
-      select_zone City
+      zone_name="City"
       ;;
     CoachKen)
-      select_zone City
+      zone_name="City"
       drops_gear=0
       ;;
 
     Moliere)
-      select_zone City
+      zone_name="City"
       drops_gear=0
       ;;
     Jamshid)
-      select_zone City
+      zone_name="City"
       ;;
 
     Cleaner)
-      select_zone Megamall
+      zone_name="Megamall"
       drops_gear=0
       ;;
     Radiosaurus)
-      select_zone Megamall
+      zone_name="Megamall"
       ;;
+
     Miriam)
-      select_zone Megamall
+      zone_name="Megamall"
       drops_gear=0
       ;;
     Slam)
-      select_zone Megamall
+      zone_name="Megamall"
       ;;
 
     Oscar)
-      select_zone Megamall
+      zone_name="Megamall"
       ;;
     Sentry)
-      select_zone Megamall
+      zone_name="Megamall"
       drops_gear=0
       ;;
 
     Siphon)
-      select_zone AmusementPark
+      zone_name="AmusementPark"
       drops_gear=0
       ;;
     Bearhunter)
-      select_zone AmusementPark
+      zone_name="AmusementPark"
       drops_gear=0
       ;;
 
     BillyStrongman)
-      select_zone AmusementPark
+      zone_name="AmusementPark"
       drops_gear=0
       ;;
     Cassandra)
-      select_zone AmusementPark
+      zone_name="AmusementPark"
       ;;
 
     GuestPerformer)
-      select_zone AmusementPark
+      zone_name="AmusementPark"
       ;;
     Momentalist)
-      select_zone AmusementPark
+      zone_name="AmusementPark"
       ;;
 
     AuntJulia)
-      select_zone Asylum
+      zone_name="Asylum"
       drops_gear=0
       ;;
     Nurse)
-      select_zone Asylum
+      zone_name="Asylum"
       ;;
 
     Unknown_86_1)
-      select_zone Asylum
+      zone_name="Asylum"
       ;;
     Unknown_86_2)
-      select_zone Asylum
+      zone_name="Asylum"
       ;;
 
     Unknown_90_1)
-      select_zone Asylum
+      zone_name="Asylum"
       ;;
     Unknown_90_2)
-      select_zone Asylum
+      zone_name="Asylum"
       ;;
 
     *)
@@ -345,53 +311,23 @@ function start_cadaver
       ;;
   esac
 
-  swipe_start=$(eval "echo \$${cadaver^^}_SWIPE_START")
-  swipe_end=$(eval "echo \$${cadaver^^}_SWIPE_END")
-  click_location=$(eval "echo \$${cadaver^^}_LOCATION")
-
-  battle_actions=()
-
-  if [ -n "$swipe_start" ]
+  if select_zone $zone_name
   then
-    battle_actions+=('
-      condition="test_game_contents home_flag.png $TEST_HOME_CENTRED ||
-                 test_game_contents home_flag_shrouded.png $TEST_HOME_CENTRED"
-      sqmessage="Bringing cadaver location into view"
-      action="xdo_and_return mousemove $swipe_start mousedown 1 mousemove $swipe_end mouseup 1"
-    ')
+    swipe_start=$(eval "echo \$${cadaver^^}_SWIPE_START")
+    swipe_end=$(eval "echo \$${cadaver^^}_SWIPE_END")
+    click_location=$(eval "echo \$${cadaver^^}_LOCATION")
+    starting_cadaver=1
+    clearwait=0
   fi
+}
 
-  # 
-  battle_actions+=('
-    condition="test_game_contents outofmission_check.png $TEST_OUT_OF_MISSION"
-    sqmessage="Clicking cadaver location"
-    action="xdo_and_return mousemove $click_location click 1"
-  ')
-
-  if [ "$drops_gear" == "1" ]
+function goto_downtown_citadel
+{
+  # If we're already Downtown, go ahead and click the citadel.  Otherwise
+  # we'll get called again once the zone-selection is complete.
+  if select_zone Downtown
   then
-    battle_actions+=('
-      condition="test_game_contents cadaver_attack.png $TEST_CADAVER_ATTACK_GEARDROP"
-      sqmessage="Attacking with gear"
-      action="xdo_and_return mousemove $CLICK_CADAVER_ATTACK_GEARDROP click 1"
-    ')
-  else
-    battle_actions+=('
-      condition="test_game_contents cadaver_attack_small.png $TEST_CADAVER_ATTACK_NOGEAR"
-      sqmessage="Attacking with gear"
-      action="xdo_and_return mousemove $CLICK_CADAVER_ATTACK_NOGEAR click 1"
-    ')
+    xdo_and_return mousemove $CLICK_DOWNTOWN_CITADEL click 1
   fi
-
-  battle_actions+=('
-    condition="test_game_contents cadaver_start_battle.png $TEST_CADAVER_STARTNEW"
-    sqmessage="Starting cadaver battle"
-    action="xdo_and_return mousemove $CLICK_CADAVER_START_NEW click 1"
-    finalaction=1
-  ')
-
-  battle_actions+=("${interrupt_actions[@]}")
-
-  run_actions battle_actions[@] true
 }
 
